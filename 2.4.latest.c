39733b29ed5afd4ac049d7462839d1f67737425a
murulhuda789 <hudasallehudin789@gmail.com>
2020-10-24 23:31:36 +0800

    commit for 2.4.latest.c

---
+++ 2.4.latest.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask(){
+  for(int i = 1; i < 5; i++) {
+  printf("\nEnter Name : ");
+  scanf("%s",&name);
+  printf("\n Name : %s\n");
+  }
+}
+
+void parentTask(){
+  printf("\nJob is done\n");
+}
+int main(void) {
+    char name[10];
+    pid_t pid = fork();
+ 
+    if(pid == 0) {
+     childTask();
+     exit(EXIT_SUCCESS);
+    }
+    else if(pid > 0) {
+    wait(NULL);
+    parentTask();
+    }
+
+    else {
+    printf("Unable to create child process.");
+    }
+
+  
+ 
+  return EXIT_SUCCESS;
+}
+
